ab15a89d961fd8bb45072a0d9d28ed31ff4dcd8b
Xin Qian <xeniaqian94@gmail.com>
2018-06-11 21:02:18 +0800

    debug utf-8 encoding on 70k paper csv

--- snorkel/parser/doc_preprocessors.py
+++ snorkel/parser/doc_preprocessors.py
@@ -75,15 +75,16 @@ class TSVDocPreprocessor(DocPreprocessor):
     """Simple parsing of TSV file with one (doc_name <tab> doc_text) per line"""
 
     def parse_file(self, fp, file_name):
-        with codecs.open(fp, encoding=self.encoding) as tsv:
+        with codecs.open(fp, encoding=self.encoding,errors='ignore') as tsv:
             for line in tsv:
-                (doc_name, doc_text) = line.split('\t')
-                stable_id = self.get_stable_id(doc_name)
-                doc = Document(
-                    name=doc_name, stable_id=stable_id,
-                    meta={'file_name': file_name}
-                )
-                yield doc, doc_text
+                if len(line.split("\t"))==2:
+                    (doc_name, doc_text) = line.split('\t')
+                    stable_id = self.get_stable_id(doc_name)
+                    doc = Document(
+                        name=doc_name, stable_id=stable_id,
+                        meta={'file_name': file_name}
+                    )
+                    yield doc, doc_text
 
 
 class TextDocPreprocessor(DocPreprocessor):
